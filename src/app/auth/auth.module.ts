e37199fb00a5ae8cb7229a80e84fe431f6abbfc2
shubham402error <dhekolkarshubham@gmail.com>
2022-06-17 10:19:41 +0530

    updated

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { MaterialModule } from './Material.module';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { RegistrationComponent } from './registration/registration.component';
     AuthRoutingModule,
     ReactiveFormsModule,
     FormsModule,
+    MaterialModule
   ]
 })
 export class AuthModule { }
